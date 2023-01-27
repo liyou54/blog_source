311166df4b0c8fdfa5cee3ac6324683f3b866ffc
liyou54 <864757331@qq.com>
2023-01-27 18:45:59 +0800

    fix bug

--- themes/shoka/source/js/_app/player.js
+++ themes/shoka/source/js/_app/player.js
@@ -1,7 +1,7 @@
 var NOWPLAYING = null
 const isMobile = /mobile/i.test(window.navigator.userAgent);
 const mediaPlayer = function(t, config) {
-  var option = {·
+  var option = {
     type: 'audio',
     mode: 'random',
     btns: ['play-pause', 'music'],
